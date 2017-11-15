db0160aa30550bed55aa13dff65ec97eb4fe2122
John Tejeda <Teyhey@users.noreply.github.com>
2017-11-15 15:37:57 -0500

    Bingbing

---
+++ Project 3 [frontend]/src/app/app.module.ts
@@ -0,0 +1,26 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';import { AppRoutingModule } from './app-routing.module';
+import { CollapseModule } from 'ng2-bootstrap';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { OrderBy } from './orderBy.pipe';
+import { LoginComponent } from './views/login/login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    OrderBy
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CollapseModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
